5eb7ec90262338eacce65e1658478a198414339f
Vardan-bg <vartan.bg@gmail.com>
2018-06-17 15:59:40 +0400

    folders tree

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Get, Controller, Header } from '@nestjs/common';
+import { Get, Controller, Header, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import {resolve} from 'path'
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Header('Cache-Control', 'text/plain')
+
   @Get()
-  root(): string {
-    return this.appService.root();
+  root(@Res() response): void {
+    // the homepage will load our index.html which contains angular logic
+    response.sendFile(resolve('./App/src/index.html'));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+
+  app.useStaticAssets(__dirname + '/App/src');
+//  app.setBaseViewsDir(__dirname + '/views');
+  app.setViewEngine('hbs');
   await app.listen(3000);
 }
 bootstrap();
